40424410cd388ca4c2aeafb179fafc85fff87e35
Andre M Ribeiro-dos-Santos <andremrsantos@gmail.com>
2021-02-19 09:44:07 -0500

    add sim_expression and sim_from_data

---
+++ R/simulate.R
@@ -0,0 +1,84 @@
+#' Simulates a single-cell experiment based on our model
+#'
+#' It simulates an experiment based on our model assumptions
+#'
+#' @export
+sim_expression <- function(cell_dp, gene_ps, log2_fc = 0L, theta = 100L) {
+  validate_numeric_vector(cell_dp)
+  validate_numeric_vector(gene_ps)
+  validate_numeric(log2_fc)
+  validate_numeric_vector(theta)
+
+  if (is.matrix(log2_fc)) {
+    if (nrow(log2_fc) != length(gene_ps) || ncol(log2_fc) != length(cell_dp)) {
+      stop(
+        "Incompatible dimensions. When `log2_fc` is a matrix, it must have ",
+        "the same number of rows as `len(gene_ps)` and columns as ",
+        "`len(cell_dp)`", call. = FALSE)
+    }
+    sim_mu <- (gene_ps %o% cell_dp) * (2 ** log2_fc)
+  } else if (length(log2_fc) == length(cell_dp) || length(log2_fc) == 1L) {
+    sim_mu <- t((cell_dp %o% gene_ps) * (2 ** log2_fc))
+  } else {
+    stop(
+      "Incompatible dimensions. `log2_fc` must be a scalar or vector of ",
+      "the same length as `cell_dp`", call. = FALSE)
+  }
+  sim_nb <- theta / (theta + sim_mu)
+  return(matrix(
+    rnbinom(length(sim_mu), size = theta, prob = sim_nb),
+    nrow = length(gene_ps), ncol = length(cell_dp)
+  ))
+}
+
+#' @importFrom DelayedMatrixStats colSums2 rowSums2
+#' @export
+sim_from_data <- function(Y, log2_fc = 0L, theta = 100L) {
+  if (!is_matrix(Y)) { stop("`Y` must be a matrix or sparseMatrix") }
+  dp <- colSums2(Y)
+  ps <- rowSums2(Y) / sum(dp)
+  sim_expression(dp, ps, log2_fc, theta = theta)
+}
+
+#' @importFrom DelayedMatrixStats colSums2 rowSums2
+#' @export
+sim_from_data_as_exp_range <-
+  function(Y, log2_fc = 0L, theta = 100L, n_genes = 100L) {
+    if (!is_matrix(Y)) { stop("`Y` must be a matrix or sparseMatrix") }
+    dp <- colSums2(Y)
+    ps <- rowSums2(Y) / sum(dp)
+    sim_ps <- seq_log(min(ps), max(ps), length_out = n_genes)
+    sim_effect(dp, sim_ps, log2_fc, theta = theta)
+  }
+
+#' @export
+sim_lfc_matrix <- function(log2_fc, clone_size, ncell, rep_each = 1L) {
+  ## validation
+  if (!is.numeric(log2_fc)) { stop("`log2_fc` must be a numeric value") }
+  if (!is.integer(clone_size) && any(clone_size > max(ncell))) {
+    stop("`clone_size` must be a integer value below or equal to ncell")
+  }
+  if (!is.integer(ncell) && length(ncell) != 1L && ncell <= 0) {
+    stop("`ncell` must be a integer scalar bigger than 0")
+  }
+  if (!is.integer(rep_each) && all(rep_each <= 0)) {
+    stop("`rep_each` must be positive integer bigger than 0")
+  }
+  if (length(log2_fc) != length(clone_size) && length(clone_size) != 1L) {
+    stop("Incompatible sizes, `log2_fc` and `clone_size` must have the same length")
+  }
+  if (length(log2_fc) != length(rep_each) && length(rep_each) != 1L) {
+    stop("Incompatible sizes, `log2_fc` and `rep_each` must have the same length")
+  }
+  ## fix values for compatibility
+  if (length(clone_size) == 1L) {
+    clone_size <- rep(clone_size, length(log2_fc))
+  }
+  if (length(rep_each) == 1L) {
+    rep_each <- rep(rep_each, length(log2_fc))
+  }
+  do.call(rbind, lapply(seq_along(log2_fc), function(i) {
+    vec <- sample(rep(c(0, log2_fc[i]), c(n - clone_size[i], clone_size[i])))
+    t(matrix(vec , length(vec) , rep_each[i]))
+  }))
+}
---
+++ tests/testthat/test-sim.R
@@ -0,0 +1,104 @@
+ncell <- 100L
+ngene <- 200L
+dp <- sample(c(10436, 52619, 32060, 18266, 8254), ncell, replace = TRUE)
+ps <- seq(0.01, 0.05, length.out = ngene)
+
+
+check_sim <- function(sim) {
+  sim_ps <- rowSums(sim) / sum(dp)
+  expect_equal(is.matrix(sim), TRUE)
+  expect_equal(ncol(sim), length(dp))
+  expect_equal(nrow(sim), length(ps))
+  expect_equal(mean((sim_ps - ps)**2), 0, tolerance = 1e-4)
+}
+
+test_that("sim_expression() parameters type requirements", {
+  unsupported_types <- list("a", FALSE, list(1), matrix(1, nrow = 2))
+
+  test_type_requirement(unsupported_types, "must be a numeric vector",
+    sim_expression,
+    gene_ps = ps, log2_fc = 0L
+  )
+  test_type_requirement(unsupported_types, "must be a numeric vector",
+    sim_expression,
+    cell_dp = dp, log2_fc = 0L
+  )
+  test_type_requirement(unsupported_types, "must be a numeric vector",
+    sim_expression,
+    cell_dp = dp, gene_ps = ps, log2_fc = 0L
+  )
+  test_type_requirement(unsupported_types[1:3], "must be a numeric",
+    sim_expression,
+    cell_dp = dp, gene_ps = ps
+  )
+})
+
+test_that("sim_expression() parameters dimension requirements", {
+  ## LOG2_FC SCALAR or N-Vector or MN-Matrix
+  scalar_lfc <- 0L
+  check_sim(sim_expression(dp, ps, log2_fc = scalar_lfc))
+  vector_lfc <- rep(0, ncell)
+  check_sim(sim_expression(dp, ps, log2_fc = vector_lfc))
+  matrix_lfc <- matrix(0, nrow = ngene, ncol = ncell)
+  check_sim(sim_expression(dp, ps, log2_fc = matrix_lfc))
+  ## Check size consistency
+  expect_error(
+    sim_expression(dp, ps, log2_fc = rep(0, ncell - 5)),
+    "Incompatible dimensions"
+  )
+  expect_error(
+    sim_expression(dp, ps, log2_fc = matrix(0, nrow = ngene, ncol = ncell - 5)),
+    "Incompatible dimensions"
+  )
+  expect_error(
+    sim_expression(dp, ps, log2_fc = matrix(0, nrow = ngene - 5, ncol = ncell)),
+    "Incompatible dimensions"
+  )
+})
+
+test_that("sim_expression() produce desired log2 fold-change", {
+  estimate_lfc <- function(sim, lfc) {
+    lfc0 <- lfc == 0
+    lfc1 <- lfc != 0
+    ps0 <- rowSums(sim[, lfc0, drop = FALSE]) / sum(dp[lfc0])
+    ps1 <- rowSums(sim[, lfc1, drop = FALSE]) / sum(dp[lfc1])
+    ps1 / ps0
+  }
+
+  for (lfc in c(-2, -1, -.5, .5, 1, 2)) {
+    lfc_vec <- rep(c(0, lfc), c(ncell - 10, 10))
+    ## Vector lfc
+    sim_mtx <- sim_expression(dp, ps, log2_fc = lfc_vec)
+    expect_equal(
+      estimate_lfc(sim_mtx, lfc_vec), rep(2**lfc, ngene),
+      tolerance = 0.2
+    )
+    ## Matrix lfc
+    lfc_mtx <- t(replicate(ngene, sample(lfc_vec)))
+    sim_mtx <- sim_expression(dp, ps, log2_fc = lfc_mtx)
+    est_lfc <- sapply(seq_len(ngene), function(i) {
+      estimate_lfc(sim_mtx[i, , drop = FALSE], lfc_mtx[i, ])
+    })
+    expect_equal(est_lfc, rep(2**lfc, ngene), tolerance = 0.2)
+  }
+})
+
+test_that("sim_from_data() requires matrix or sparseMatrix", {
+  unsupported_types <- list(1, "a", FALSE, list(1), c(1, 2, 3))
+
+  test_type_requirement(
+    unsupported_types, "must be a matrix or sparseMatrix", sim_from_data
+  )
+})
+
+test_that("sim_from_data() replicates similar data from base", {
+  data("scSimulated")
+  sim_mtx <- sim_from_data(scSimulated)
+  expect_equal(dim(sim_mtx), dim(scSimulated))
+  expect_equal(rowMeans(sim_mtx), rowMeans2(scSimulated), tolerance = 0.1)
+  ## Small lfc change
+  lfc_vec <- rep(c(0, 1), c(ncol(scSimulated) - 10, 10))
+  sim_mtx <- sim_from_data(scSimulated, lfc_vec)
+  expect_equal(dim(sim_mtx), dim(scSimulated))
+  expect_equal(rowMeans(sim_mtx), rowMeans2(scSimulated), tolerance = 0.1)
+})
