6e36f1291e44a3bba087c7e787a4b77edffa600c
Andre M Ribeiro-dos-Santos <andremrsantos@gmail.com>
2021-06-04 11:32:14 -0400

    add p_method to allow user to choose their p-value adjustment and consolidate estimate_theta function

--- R/decal.R
+++ R/decal.R
@@ -33,6 +33,8 @@
 #' cells (`x0`) required.
 #' @param gene_col gene index column name in `perturbations`
 #' @param clone_col clone index column name in `perturbations`
+#' @param p_method p-value adjustment for multiple comparisons.
+#' See `\link[stats]{p.adjust}`.
 #' @return it extends `perturbations` table adding the following columns:
 #' - `n0` and `n1`: number of non-perturbed and perturbed cells
 #' - `x0` and `x1`: number of non-perturbed and perturbed cells average count
@@ -48,7 +50,7 @@
 #' @export
 decal <- function(perturbations, count, clone, theta_sample = 2000,
                   min_mu = 0.05, min_n = 3, min_x = 1,
-                  gene_col = "gene", clone_col = "clone") {
+                  gene_col = "gene", clone_col = "clone", p_method = "BH") {
   ## Validate input
   validate_dataframe(perturbations, c(gene_col, clone_col))
   validate_matrix(count)
@@ -93,7 +95,11 @@ decal <- function(perturbations, count, clone, theta_sample = 2000,
   if (length(which_test) == 0) {
     warning("No gene & clone perturbation matched the required criteria.")
   } else {
-    fit <- fit_nb(count, clone, theta, log_depth, rowidx[which_test], colidx[which_test])
+    fit <- fit_nb(
+      count, clone, theta, log_depth,
+      rowidx[which_test], colidx[which_test],
+      p_method
+    )
     perturbations[which_test, names(fit)] <- fit
   }
   return(perturbations)
@@ -108,7 +114,8 @@ build_clone_matrix <- function(clone, cells) {
   }
   if (any(!is_index(unlist(clone)))) {
     stop("`clone` cells list must be integer or character indexes",
-         call. = FALSE)
+      call. = FALSE
+    )
   }
   validate_index(cells)
   ## Vectorize clone list
@@ -116,79 +123,75 @@ build_clone_matrix <- function(clone, cells) {
   vec_cells <- unlist(clone)
   vec_clone <- rep(clones, sapply(clone, length))
   ## Build matrix
-  mtx <- sparseMatrix(i = match(vec_cells, cells), j = match(vec_cells, clones), x = 1L, dims = c(
-    length(cells),
-    length(clones)
-  ))
-  if (is.character(cells)) {
-    rownames(mtx) <- cells
-  }
-  if (is.character(clones)) {
-    colnames(mtx) <- clones
-  }
+  mtx <- sparseMatrix(
+    i = match(vec_cells, cells), j = match(vec_cells, clones), x = 1L,
+    dims = c(length(cells), length(clones))
+  )
+  if (is.character(cells)) rownames(mtx) <- cells
+  if (is.character(clones)) colnames(mtx) <- clones
   return(mtx)
 }
 
 #' @importFrom fastglm fastglm
 #' @importFrom MASS theta.ml
 #' @importFrom stats poisson
-#'
 #' @noRd
-estimate_theta_raw <- function(count, genes, log_dp) {
-  fit_poisson <- function(i) {
+estimate_theta_raw <- function(count, genes, log_dp, floor = 1E-7) {
+  Y <- as.matrix(count[genes, ])
+  theta <- sapply(seq_along(genes), FUN = function(i) {
     y <- Y[i, ]
     x <- matrix(1L, nrow = length(y))
     f <- fastglm(x, y, family = poisson(), method = 2, offset = log_dp)
-    as.numeric(theta.ml(y, f$fitted.values))
-  }
-  Y <- as.matrix(count[genes, ])
-
-  theta <- suppressWarnings(sapply(seq_along(genes), FUN = fit_poisson))
-  theta <- pmax(theta, 1e-07)
+    suppressWarnings(as.numeric(theta.ml(y, f$fitted.values)))
+  })
+  return(pmax(theta, floor))
 }
 
-#' @importFrom stats approx bw.SJ density ksmooth predict
-#'
+#' @importFrom stats bw.SJ ksmooth
+#' @noRd
+regularize_theta <- function(logmu1, theta1, logmu) {
+  odds1 <- log10(1 + 10**logmu1 / theta1)
+  xpnts <- pmin(pmax(logmu, min(logmu1)), max(logmu1))
+  odds <- numeric(length(logmu))
+  odds[order(xpnts)] <- ksmooth(
+    x = logmu1, y = odds1, x.points = xpnts,
+    bandwidth = bw.SJ(logmu1) * 3, kernel = "normal"
+  )$y
+  return(10**xpnts / (10**odds - 1))
+}
+
+#' @importFrom stats approx density
 #' @noRd
 estimate_theta <- function(count, mu, log_dp, n, genes) {
-  ## Response vector
-  theta <- numeric(nrow(count))
   ## Subsample
-  if (is.null(genes)) {
-    genes <- which(mu > 0)
-  }
-  log_mu <- log(mu)[genes]
-  ## Step1. Estimate theta in subsample TODO: subsample cols TODO: run estimate_theta_raw
-  ## in blocks to avoid memory overflow
+  genes <- or(genes, which(mu > 0))
+  logmu <- log(mu)[genes]
+  ## Step1. Estimate theta in subsample
+  ## TODO: subsample cols
+  ## TODO: run estimate_theta_raw in blocks to avoid memory overflow
   if (n < length(genes)) {
-    dens <- density(x = log_mu, bw = "nrd", adjust = 1)
-    prob <- 1 / (approx(dens$x, dens$y, xout = log_mu)$y + .Machine$double.eps)
-    gene1 <- sample(genes, size = n, prob = prob)
-    log_mu1 <- log(mu)[gene1]
+    dens <- density(x = logmu, bw = "nrd", adjust = 1)
+    prob <- 1 / (approx(dens$x, dens$y, xout = logmu)$y + .Machine$double.eps)
+    genes1 <- sample(genes, size = n, prob = prob)
+    logmu1 <- log(mu)[genes1]
   } else {
-    gene1 <- genes
-    log_mu1 <- log_mu
+    genes1 <- genes
+    logmu1 <- logmu
   }
   theta1 <- estimate_theta_raw(count, genes, log_dp)
-  ## Step2. Regularize theta estimate TODO: remove outliers?
-  odds1 <- log10(1 + 10^log_mu1 / theta1)
-  xs <- pmin(pmax(log_mu, min(log_mu1)), max(log_mu1))
-  odds <- numeric(length(log_mu))
-  odds[order(xs)] <- ksmooth(x = log_mu1, y = odds1, x.points = xs, bandwidth = bw.SJ(log_mu1) *
-    3, kernel = "normal")$y
-  theta[genes] <- 10^xs / (10^odds - 1)
+  ## Step2. Regularize theta estimate
+  ## TODO: remove outliers?
+  theta <- numeric(nrow(count))
+  theta[genes] <- regularize_theta(logmu1, theta1, logmu)
   return(theta)
 }
 
 #' @importFrom fastglm fastglm
 #' @importFrom MASS negative.binomial
-#' @importFrom stats p.adjust
-#'
+#' @importFrom stats p.adjust predict
 #' @noRd
-fit_nb <- function(count, clone, theta, log_dp, rows, cols) {
-  if (length(theta) == 1L) {
-    theta <- rep(theta, nrow(Y))
-  }
+fit_nb <- function(count, clone, theta, log_dp, rows, cols, p_method) {
+  if (length(theta) == 1L) theta <- rep(theta, nrow(Y))
   if (length(rows) != length(cols)) {
     stop("rows and cols index must have the same length", call. = FALSE)
   }
@@ -209,12 +212,12 @@ fit_nb <- function(count, clone, theta, log_dp, rows, cols) {
     coef <- summary(f)$coef[3, ]
     names(coef) <- NULL
     c(
-      xb = predict(f, cbind(1, 1), type = "response") * mean_depth, z = coef[3], lfc = coef[1] / ln2,
-      pvalue = coef[4]
+      xb = predict(f, cbind(1, 1), type = "response") * mean_depth,
+      z = coef[3], lfc = coef[1] / ln2, pvalue = coef[4]
     )
   }, match(rows, ix), match(cols, jx))
   result <- as.data.frame(t(result))
   ## Compute Benjamin-Hochenberg p-value adjustment
-  result$p_adjusted <- p.adjust(result$pvalue, "BH")
+  result$p_adjusted <- p.adjust(result$pvalue, p_method)
   return(result)
 }
