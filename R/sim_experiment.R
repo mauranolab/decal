8a8a278002c041eebae902751ec16ccc64c7e99d
André Ribeiro dos Santos <andreribeirodossantos@macbookdeandre.sb.nyumc.org>
2021-06-17 12:15:48 -0400

    add sim_experiment and sim_experiment_from_data

---
+++ R/sim_experiment.R
@@ -0,0 +1,108 @@
+#' Simulate a DECAL perturbation experiment
+#'
+#' It simulates a random DECAL experiment generating a random UMI count matrix
+#' with cells randomly assigned to different clones and introducing to each
+#' clone perturbations to random genes.
+#'
+#' @param lfc vector indicating the perturbations to be generated for each clone
+#' @param nclones number of clones to be simulated
+#' @param min_n minimum clone size
+#' @param max_n maximum clone size
+#' @param theta negative binomial dispersion parameter
+#' @return a list containing the following fields:
+#' - `perturbations`, a table indicating the perturbations `expected_lfc` for
+#'    a `gene` in a `clone`.
+#' - `clone`, a list of cells assigned to each clone.
+#' - `count`, a random UMI count matrix.
+#'
+#' @name simulate_experiment
+NULL
+
+#' @rdname simulate_experiment
+#'
+#' @param depth
+#' @param ratio
+#' @export
+sim_experiment <- function(depth, ratio, lfc=0, nclones=10L, min_n=2L, max_n=20L, theta = 100L) {
+  validate_positive_integer(depth)
+  validate_rate(ratio)
+  validate_numeric(lfc)
+  validate_positive_integer_scalar(nclones)
+  validate_positive_integer_scalar(min_n)
+  validate_positive_integer_scalar(max_n)
+  validate_numeric(theta)
+
+  ncells <- length(depth)
+  ngenes <- length(ratio)
+  ## Assign to clones
+  clone <- sim_clone_range(nclones, ncells, min_n, max_n)
+  ## Simulate random perturbations
+  perturbations <- simulate_perturbations(nclones, ngenes, lfc)
+  ## produce count matrix
+  lfc_mat <- build_lfc_mat(perturbations, clone, ngenes, ncells)
+  count <- sim_count(depth, ratio, lfc = lfc_mat, theta = theta)
+  return(list(perturbations = perturbations, clone = clone, count = count))
+}
+
+#' @rdname simulate_experiment
+#'
+#' @param reference
+#' @param ngenes
+#' @export
+sim_experiment_from_data <- function(reference, lfc=0, nclones=10L, min_n=2L, max_n=20L, theta = 100L, ngenes = NULL) {
+  validate_matrix(reference)
+  validate_numeric(lfc)
+  validate_positive_integer_scalar(nclones)
+  validate_positive_integer_scalar(min_n)
+  validate_positive_integer_scalar(max_n)
+  validate_numeric(theta)
+
+  ncells  <- ncol(reference)
+  ngenes_ <- nrow(reference)
+  if (!is.null(ngenes)) {
+    validate_positive_integer_scalar(ngenes)
+    ngenes_ <- ngenes
+  }
+
+  clone <- sim_clone_range(nclones, ncells, min_n, max_n)
+  perturbations <- simulate_perturbations(nclones, ngenes_, lfc)
+  ## Build count matrix
+  lfc_mat <- build_lfc_mat(perturbations, clone, ngenes_, ncells)
+  count <- sim_count_from_data(reference, lfc_mat, theta = theta, ngenes = ngenes)
+
+  return(list(perturbations = perturbations, clone = clone, count = count))
+}
+
+#' @noRd
+build_lfc_mat <- function(perturbations, clone, nrow, ncol) {
+  clone_mat <- build_clone_matrix(clone, seq_len(ncol))
+  lfc_mat <- matrix(0, nrow = nrow, ncol = ncol)
+  for (i in seq_len(nrow(perturbations))) {
+    clone_i <- perturbations$clone[i]
+    gene_i  <- perturbations$gene[i]
+    lfc_i   <- perturbations$expected_lfc[i]
+    ## WARN PRE-ASSIGNED PERTURBATION WAS OVERWRITTEN DUE TWO CLONES
+    ## PERTUBATING THE SAME GENE
+    x <- clone_mat[, clone_i] == 1
+    if (any(lfc_mat[gene_i, x] != 0)) {
+      warning("Two or more clones pertubated the same gene.",
+              "The intended log2 fold-change may be affected.")
+    }
+    lfc_mat[gene_i,] <- lfc_mat[gene_i,] + x * lfc_i
+  }
+  return(lfc_mat)
+}
+
+#' @noRd
+simulate_perturbations <- function(nclones, ngenes, lfc) {
+  genes <- seq_len(ngenes)
+  npert <- length(lfc)
+  if (npert > ngenes) {
+    stop("number of perturbations exceed the total number of genes", call. = FALSE)
+  }
+  data.frame(
+    clone = rep(seq_len(nclones), each = npert),
+    gene  = as.vector(replicate(nclones, sample(genes, npert))),
+    expected_lfc = rep(lfc, times = nclones)
+  )
+}
---
+++ tests/testthat/test-sim_experiment.R
@@ -0,0 +1,130 @@
+ncell <- 100L
+ngene <- 200L
+depth <- floor(rlnorm(ncell, 9, .5))
+ratio <- seq(0.01, 0.05, length.out = ngene)
+lfc <- rep(c(-2, -.5, 0, .5, 2), c(2, 2, 5, 2, 2))
+
+estimate_lfc <- function(count, x) {
+  r0 <- rowSums(count[,!x, drop=FALSE]) / sum(depth[!x])
+  r1 <- rowSums(count[, x, drop=FALSE]) / sum(depth[x])
+  return(log2(r1/r0))
+}
+
+test_that("sim_experiment() outputs the expected output", {
+  res <- sim_experiment(depth, ratio, lfc, nclones = 10L, min_n = 5, max_n = 20)
+  expect_named(res, c("perturbations", "clone", "count"))
+  ## Check count
+  count <- res$count
+  expect_equal(dim(count), c(ngene, ncell))
+  expect_equal(var(rowSums(count)/sum(count) - ratio), 0, tolerance = 1e-4)
+  ## Check clone
+  clone <- res$clone
+  sizes <- sapply(clone, length)
+  expect_type(clone, "list")
+  expect_equal(length(clone), 10L)
+  expect_true(min(sizes) >= 5)
+  expect_true(max(sizes) <= 20)
+  expect_true(all(unlist(clone) %in% seq_len(ncell)))
+  ## Check perturbations
+  pert <- res$perturbations
+  expect_named(pert, c("clone", "gene", "expected_lfc"))
+  expect_equal(nrow(pert), 10 * length(lfc))
+  expect_true(all(pert$clone >= 1 & pert$clone <= 10L))
+  expect_true(all(pert$gene >= 1 & pert$gene <= ngene))
+  ## Evaluate perturbation effect
+  for (ci in seq_len(10L)) {
+    xi <- seq_len(ncell) %in% clone[[ci]]
+    gi <- pert$gene[pert$clone == ci]
+    expect_equal(estimate_lfc(count[gi,], xi), lfc, tolerance = 1)
+  }
+})
+
+test_that("sim_experiment_from_data() outputs the expected output", {
+  ref <- ratio %o% depth
+  rat <- 10 ** (seq(log10(min(ratio)), log10(max(ratio)), length.out = 500L))
+
+  res <- sim_experiment_from_data(ref, lfc, nclones = 10L, min_n = 5, max_n = 20, ngenes = 500L)
+  expect_named(res, c("perturbations", "clone", "count"))
+  ## Check count
+  count <- res$count
+  expect_equal(dim(count), c(500L, ncell))
+  expect_equal(var(rowSums(count)/sum(count) - rat), 0, tolerance = 1e-3)
+  ## Check clone
+  clone <- res$clone
+  sizes <- sapply(clone, length)
+  expect_type(clone, "list")
+  expect_equal(length(clone), 10L)
+  expect_true(min(sizes) >= 5)
+  expect_true(max(sizes) <= 20)
+  expect_true(all(unlist(clone) %in% seq_len(ncell)))
+  ## Check perturbations
+  pert <- res$perturbations
+  expect_named(pert, c("clone", "gene", "expected_lfc"))
+  expect_equal(nrow(pert), 10 * length(lfc))
+  expect_true(all(pert$clone >= 1 & pert$clone <= 10L))
+  expect_true(all(pert$gene >= 1 & pert$gene <= 500L))
+  ## Evaluate perturbation effect
+  for (ci in seq_len(10L)) {
+    xi <- seq_len(ncell) %in% clone[[ci]]
+    gi <- pert$gene[pert$clone == ci]
+    expect_equal(estimate_lfc(count[gi,], xi), lfc, tolerance = 1)
+  }
+})
+
+test_that("sim_experiment fails when request more perturbations than genes", {
+  expect_error(
+    sim_experiment(depth, seq(0.01, 0.05, length.out=10), lfc = rep(0, 20)),
+    "number of perturbations exceed the total number of genes"
+  )
+})
+
+test_that("sim_experiment validates parametrs", {
+  unsupported <- list("A", TRUE, list(1, 2))
+  for (val in unsupported) {
+    expect_error(sim_experiment(val, ratio), "must be a positive integer")
+    expect_error(sim_experiment(depth, val), "must be numeric")
+    expect_error(sim_experiment(depth, ratio, lfc = val), "must be numeric")
+    expect_error(sim_experiment(depth, ratio, theta = val), "must be numeric")
+    expect_error(sim_experiment(depth, ratio, nclones = val), "must be a positive integer scalar")
+    expect_error(sim_experiment(depth, ratio, min_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment(depth, ratio, max_n = val), "must be a positive integer scalar")
+  }
+  ## check positive integer
+  for (val in c(-1, 0, 1.5)) {
+    expect_error(sim_experiment(val, ratio), "must be a positive integer")
+    expect_error(sim_experiment(depth, ratio, nclones = val), "must be a positive integer scalar")
+    expect_error(sim_experiment(depth, ratio, min_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment(depth, ratio, max_n = val), "must be a positive integer scalar")
+  }
+  ## check scalar
+  expect_error(sim_experiment(depth, ratio, nclones = c(1, 2)), "must be a positive integer scalar")
+  expect_error(sim_experiment(depth, ratio, min_n = c(1, 2)), "must be a positive integer scalar")
+  expect_error(sim_experiment(depth, ratio, max_n = c(1, 2)), "must be a positive integer scalar")
+  ## check rate
+  expect_error(sim_experiment(depth, -1), "must be a ratio between 0 and 1")
+  expect_error(sim_experiment(depth, 2), "must be a ratio between 0 and 1")
+})
+
+
+test_that("sim_experiment_from_data validates parametrs", {
+  ref <- ratio %o% depth
+  unsupported <- list("A", TRUE, list(1, 2))
+  for (val in unsupported) {
+    expect_error(sim_experiment_from_data(val), "must be a matrix")
+    expect_error(sim_experiment_from_data(ref, lfc = val), "must be numeric")
+    expect_error(sim_experiment_from_data(ref, theta = val), "must be numeric")
+    expect_error(sim_experiment_from_data(ref, nclones = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, min_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, max_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, ngenes = val), "must be a positive integer scalar")
+  }
+  ## check positive integer scalar
+  for (val in list(-1, 0, 1.5, c(1, 2, 3))) {
+    expect_error(sim_experiment_from_data(ref, nclones = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, min_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, max_n = val), "must be a positive integer scalar")
+    expect_error(sim_experiment_from_data(ref, ngenes = val), "must be a positive integer scalar")
+  }
+  ## check matrix
+  expect_error(sim_experiment_from_data(c(1, 2, 3, 4, 5)), "must be a matrix")
+})
