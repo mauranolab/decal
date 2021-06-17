3a24ba086577a918c483ed5ebea7ec32e3888e21
André Ribeiro dos Santos <andreribeirodossantos@MacBook-de-Andre.local>
2021-06-17 08:37:37 -0400

    Expanded `validation` functions

--- R/validate.R
+++ R/validate.R
@@ -1,6 +1,6 @@
 #' @noRd
 is_index <- function(x) {
-  return(is.character(x) || (is.integer(x) && all(x > 0)))
+  return(is.character(x) || (all(x == as.integer(x)) && all(x > 0)))
 }
 
 #' @noRd
@@ -40,5 +40,51 @@ validate_matrix <- function(x) {
   if (!is_matrix(x)) {
     name <- deparse(substitute(x))
     stop("`", name, "` must be a matrix or sparseMatrix", call. = FALSE)
+  }
+}
+
+#' @noRd
+is_integer <- function(x) {
+  is.integer(x) || (is.numeric(x) && all(x == round(x)))
+}
+
+#' @noRd
+validate_positive_integer <- function(x) {
+  if (!(is_integer(x) && all(x > 0))) {
+    name <- deparse(substitute(x))
+    stop("`", name, "` must be a positive integer", call. = FALSE)
+  }
+}
+
+#' @noRd
+validate_positive_integer_scalar <- function(x) {
+  if (!(is_integer(x) && all(x > 0) && length(x) == 1L)) {
+    name <- deparse(substitute(x))
+    stop("`", name, "` must be a positive integer scalar", call. = FALSE)
+  }
+}
+
+#' @noRd
+validate_numeric <- function(x) {
+  if (!is.numeric(x)) {
+    name <- deparse(substitute(x))
+    stop("`", name, "` must be numeric", call. = FALSE)
+  }
+}
+
+#' @noRd
+validate_numeric_scalar <- function(x) {
+  if (!(is.numeric(x) && length(x) == 1L)) {
+    name <- deparse(substitute(x))
+    stop("`", name, "` must be a numeric scalar", call. = FALSE)
+  }
+}
+
+#' @noRd
+validate_rate <- function(x) {
+  do.call(validate_numeric, list(enquote(x)))
+  if (all(x >= 0 && x <= 1)) {
+    name <- deparse(substitute(x))
+    stop("`", name, "` must be a ratio between 0 and 1", call. = FALSE)
   }
 }
